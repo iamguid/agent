86a8b904dbba5db0f25922e5655e3aa95f7467d3
Vladislav <iam.guid@gmail.com>
2022-09-20 23:39:21 +0300

    Init melos monorepo with agent_core and agent_flutter packages

---
+++ packages/agent_flutter/example/agent_flutter_example.dart
@@ -0,0 +1,6 @@
+import 'package:agent_flutter/agent_flutter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/agent_flutter/lib/agent_flutter.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library agent_flutter;
+
+export 'src/agent_flutter_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
