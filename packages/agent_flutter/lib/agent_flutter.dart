afd3878c60ed41976ceea77169069d01c49e52f4
Vladislav <iam.guid@gmail.com>
2022-09-22 00:21:55 +0300

    Add agent_core export

--- packages/agent_flutter/lib/agent_flutter.dart
+++ packages/agent_flutter/lib/agent_flutter.dart
@@ -3,6 +3,7 @@
 /// More dartdocs go here.
 library agent_flutter;
 
+export 'package:agent_core/agent_core.dart';
 export 'src/agent_provider.dart';
 export 'src/multi_agent_provider.dart';
 export 'src/state_agent_builder.dart';
