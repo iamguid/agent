2b9e47fef850563621b1249ac84ac2c62ec8ea04
Vladislav <iam.guid@gmail.com>
2022-10-16 22:12:57 +0300

    Use agent_flutter v0.2.0 in counter example

--- examples/counter/lib/counter_events.dart
+++ examples/counter/lib/counter_events.dart
@@ -1,4 +1,6 @@
-abstract class CounterEvent {}
+import 'package:agent_flutter/agent_flutter.dart';
+
+abstract class CounterEvent extends AgentBaseEvent {}
 
 class CounterIncrementedEvent extends CounterEvent {}
 
