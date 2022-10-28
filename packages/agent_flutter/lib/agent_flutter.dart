fd2bc6416081b156e877056973047e15d2b30055
Vladislav <iam.guid@gmail.com>
2022-10-28 11:27:03 +0300

    Exports

--- packages/agent_flutter/lib/agent_flutter.dart
+++ packages/agent_flutter/lib/agent_flutter.dart
@@ -6,6 +6,8 @@ library agent_flutter;
 export 'package:agent_core/agent_core.dart'
     show Agent, StateAgent, AgentBaseEvent;
 export 'src/agent_provider.dart';
+export 'src/agent_consumer.dart';
+export 'src/agent_connector.dart';
 export 'src/multi_agent_provider.dart';
 export 'src/state_agent_builder.dart';
 export 'src/state_agent_consumer.dart';
